3ec87fe6e413b0416dfa463be6a71c7c349358de
Huanan <donghuanan@inston.ltd>
2026-01-21 14:39:28 +0800

    chore: update minimum macOS version in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Swiftic",
-    platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
+    platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
     products: [
         .library(
             name: "Swiftic",
